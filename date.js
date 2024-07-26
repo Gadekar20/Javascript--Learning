854e61bdb236af688b87493d659cc65486b8f9b5
Gadekar20 <133889504+Gadekar20@users.noreply.github.com>
2024-07-26 21:48:04 +0530

    date,day,hour in js

---
+++ date.js
@@ -0,0 +1,6 @@
+let myDate=new Date();
+console.log(myDate.getTime());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
